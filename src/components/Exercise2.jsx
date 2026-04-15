7e6ed103bf000d3c5a40925c4fd3eb3523fafab4
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2026-04-16 00:31:45 +0900

    価格に関するコメントを追加

--- src/components/Exercise2.jsx
+++ src/components/Exercise2.jsx
@@ -5,7 +5,10 @@ const Exercise2 = () => {
   return (
     <ul>
       {devicesWithMinSize700.map((device) => (
-        <li key={device.id}>{device.name}</li>
+        <li key={device.id}>
+          {device.name}
+          {device.price >= 1000 ? "「💰 高級品」" : "「🛒 お買い得」"}
+        </li>
       ))}
     </ul>
   );
